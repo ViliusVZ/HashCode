ccaad70773fdcbfba75cc313a7c422c17709ee6b
Paulius <nar.paulius@gmail.com>
2019-02-28 20:38:59 +0200

    saddasd

--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -15,6 +15,42 @@ namespace HashCode
             //Justas
             //Tadas
 
+
+
+            //rusiavimas tagu ir skaiciavimas, Paulius
+
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Paulius\Documents\hashcode\HashCode\HashCode\b_lovely_landscapes.txt");
+
+
+            int number_of_lines = Convert.ToInt32( lines[0]);
+            System.Console.WriteLine("number of lines  = ");
+            System.Console.WriteLine(Convert.ToString(number_of_lines));
+
+            
+
+            int[] number_of_tags_in_photo = new int [1000];
+
+            for (int i = 1; i < number_of_lines; i++)
+            {
+
+            }
+
+
+
+            foreach (string line in lines)
+            {
+                
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
+
+
+
     }
+    
 }
